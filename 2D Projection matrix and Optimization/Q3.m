d290bd371dc1bb2827bae3f9fc313e1d7b623f5d
Kutay <kutay.ugurlu.1@gmail.com>
2022-06-05 15:55:25 +0300

    Q3 figure sizes adjusted, comments remaining.

--- 2D Projection matrix and Optimization/Q3.m
+++ 2D Projection matrix and Optimization/Q3.m
@@ -1,9 +1,8 @@
 %% Setup
-
 close all;
 clc;
 
-%% a
+%% Part a
 
 I = imread('cameraman.tif'); 
 X = im2double(I); 
@@ -15,8 +14,9 @@ title('Gaussian Filter')
 C = convmtx2(F,size(X,1),size(X,2));
 X_vec = X(:);
 y = C*X_vec;
-figure
-subplot(2,2,1)
+f1 = figure;
+ax = gca;
+subplot(2,2,1,ax)
 imagesc(X), colormap gray
 title('Original Phantom')
 subplot(2,2,2)
@@ -24,11 +24,11 @@ Y_blurred = reshape(y,size(F)+size(X)-1);
 imagesc(Y_blurred)
 title('Blurred Phantom')
 
-%% b
+%% Part b
 for ratio = [3 4]
-    variance = var(y)/ratio; % SNR adjustment
+    variance = var(y)/10^ratio; % SNR adjustment
     sigma_n = sqrt(variance);
-    noise = sigma_n * randn(size(y));
+    noise = sigma_n * randn(size(y)); % N~(0,sigma_n^2)
     y_n = y + noise;
     y_img = reshape(y_n,size(F)+size(X)-1);
     subplot(2,2,ratio)
@@ -36,11 +36,13 @@ for ratio = [3 4]
     title(['Blurred Phantom with SNR: ',num2str(10*ratio)])
 end
 
-%% c: Inverse Filtering in Frequency domain 
+%% Part c: Inverse Filtering in Frequency domain 
 figure
 size_Y = size(Y_blurred);
-X_inv = ifft2(fft2(Y_blurred,size_Y(1),size_Y(2))./fft2(F,size_Y(1),size_Y(2)),size_Y(1),size_Y(2));
-X_inv_noisy = ifft2(fft2(y_img)./fft2(F,size_Y(1),size_Y(2)),size_Y(1),size_Y(2));
+X_inv = ifft2(fft2(Y_blurred,size_Y(1), ...
+    size_Y(2))./fft2(F,size_Y(1),size_Y(2)),size_Y(1),size_Y(2));
+X_inv_noisy = ifft2(fft2(y_img)./fft2(F, ...
+    size_Y(1),size_Y(2)),size_Y(1),size_Y(2));
 subplot(1,2,1)
 imagesc(X_inv), colormap gray
 title('Inverse Filtering of y')
@@ -49,7 +51,7 @@ imagesc(X_inv_noisy)
 title('Inverse Filtering of y_{noise}')
 
 
-%% d: LS Filtering
+%% Part d: LS Filtering
 figure
 C = full(C); % The LS solution outputs wrong results if C remains sparse!
 X_inv = reshape(inv(C'*C)*C'*y,size(X));
@@ -61,7 +63,7 @@ subplot(1,2,2)
 imagesc(X_inv_noisy)
 title('Least Squares Filtering of y_{noise}')
 
-%% e 
+%% Part e 
 % y = U S VH x;
 % x = V 1/S UH y;
 
@@ -77,7 +79,8 @@ for i = 1:length(singular_values)
     Errors(i) = relative_error(X_vec, X_back_vec);
 end
 X_back = reshape(X_back_vec,size(X));
-figure
+f = figure;
+f.Position = [100 100 900 300];
 colormap gray
 subplot(1,3,1)
 imagesc(X)
@@ -103,7 +106,8 @@ for i = 1:length(singular_values)
 end
 Full_Errors = Errors;
 X_back = reshape(X_back_vec,size(X));
-figure
+f = figure;
+f.Position = [100 100 900 300];
 colormap gray
 subplot(1,3,1)
 imagesc(X)
@@ -117,11 +121,12 @@ title('Errors')
 sgtitle('TSVD Reconstructions for noisy image')
 
 %% Find threshold for noisy reconstruction 
-
-f = figure;
+% A ratio of $\frac{1}{200}$ in singular value amplitude provided a
+% satisfactory amount of reconstruction.
+f = figure; colormap gray;
 f.Position = [100 100 600 800];
-
-for ratio = 1:10
+plot_idx = 4;
+for ratio = 100:100:900
     max_sing_value = max(singular_values);
     threshold = max_sing_value / ratio;
     [minimum, index] = min(abs(singular_values-threshold));
@@ -136,9 +141,11 @@ for ratio = 1:10
         Errors(i) = relative_error(X_vec, X_back_vec);
     end
     X_back = reshape(X_back_vec,size(X));
-    subplot(4,3,ratio+2)
+    subplot(4,3,plot_idx)
     imagesc(X_back)
-    title({['Ratio:',num2str(ratio)],['Min Eigenvalue index: ',num2str(index)]})
+    title({['Ratio:',num2str(ratio)] ...
+        ,['Min Eigenvalue index: ',num2str(index)]})
+    plot_idx = plot_idx + 1;
 end
 
 colormap gray
@@ -147,10 +154,12 @@ imagesc(X)
 title('Original Image')
 
 subplot(4,3,2)
-plot(Full_Errors(1:250))
+plot(100:900,Full_Errors(100:900))
 title('Errors')
+xlabel('Singular Value Iteration')
+ylabel('Relative Error')
 
 subplot(4,3,3)
 plot(singular_values)
-title('Singular Value distribution')
+title('Sorted Singular Value distribution')
 sgtitle('TSVD Reconstructions for noisy image')
